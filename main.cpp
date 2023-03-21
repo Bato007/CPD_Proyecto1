c5f4ef14e750204a545581388502cf5301320cc5
Requetetin <martinamadog@gmail.com>
2023-03-20 20:45:06 -0600

    DEL - Removed unused LTexture functions

--- main.cpp
+++ main.cpp
@@ -167,21 +167,6 @@ void LTexture::free() {
 	}
 }
 
-void LTexture::setColor(Uint8 red, Uint8 green, Uint8 blue) {
-	// Modulate texture rgb
-	SDL_SetTextureColorMod(mTexture, red, green, blue);
-}
-
-void LTexture::setBlendMode(SDL_BlendMode blending) {
-	// Set blending function
-	SDL_SetTextureBlendMode( mTexture, blending );
-}
-
-void LTexture::setAlpha( Uint8 alpha ) {
-	// Modulate texture alpha
-	SDL_SetTextureAlphaMod( mTexture, alpha );
-}
-
 void LTexture::render(int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip) {
 	// Set rendering space and render to screen
 	SDL_Rect renderQuad = { x, y, mWidth, mHeight };
